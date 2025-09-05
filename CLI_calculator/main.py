1822f2f1e6c7610456bb06ced68549217edcaea9
jacobbritus <jacobbritus@gmail.com>
2025-09-05 15:36:33 +0200

    Cleaned up and added docstrings.

--- CLI_calculator/main.py
+++ CLI_calculator/main.py
@@ -4,74 +4,97 @@ import os
 
 class Calculator:
     def __init__(self):
-        self.first_number = ""
-        self.arithmetic_expression = ""
-        self.second_number = ""
-        self.result = ""
+        self.first_number: str or int or float = ""
+        self.arithmetic_expression: str = ""
+        self.second_number: str or int or float = ""
+        self.result: str or int or float = ""
 
     @staticmethod
-    def clear_terminal():
+    def clear_terminal() -> None:
+        """Clears the terminal whenever the calculator updates."""
         os.system("cls" if os.name == "nt" else "clear")
 
-    def error_message(self):
-        result_pre = self.result
+    def error_message(self) -> None:
+        """Shows an error message for a second upon invalid inputs"""
+        # this variable is set as self.result is either "" or the actual result.
+        current_result_string = self.result
         self.result = "ERROR"
-        self.print_input()
+        self.update_display()
         time.sleep(1)
-        self.result = result_pre
+        self.result = current_result_string
 
-    def update_number_input(self, user_input, attr_name):
-        num = getattr(self, attr_name)
+    def update_number_input(self, user_input, attr_name) -> bool:
+        """Reads the keyboards and updates numbers."""
+        current_value = getattr(self, attr_name)
 
-        if user_input.isdigit() or self.first_number and user_input == "." and not "." in num:
-            setattr(self, attr_name, num + user_input)
+        # === adds numbers and one decimal point ===
+        if user_input.isdigit() or self.first_number and user_input == "." and not "." in current_value:
+            setattr(self, attr_name, current_value + user_input)
             return True
-        elif num and user_input == readchar.key.BACKSPACE:
-            setattr(self, attr_name, num[:-1])
+
+        # === removes the last input ===
+        elif current_value and user_input == readchar.key.BACKSPACE:
+            setattr(self, attr_name, current_value[:-1])
             return True
+
+        # === clears current value ===
         elif user_input == "c":
             setattr(self, attr_name, "")
             return True
+
         else:
             return False
 
-    def get_first_number(self):
+    def get_first_number(self) -> None:
+        """Gets the first number."""
         while True:
             user_input = self.get_input()
-            valid = self.update_number_input(user_input, attr_name = "first_number")
+            is_valid_input = self.update_number_input(user_input, attr_name = "first_number")
+
+            # === move on to the second input ===
             if user_input in ["+", "-", "/", "*"] and self.first_number:
                 self.arithmetic_expression = user_input
                 self.first_number = self.first_number
-                self.print_input()
+                self.update_display()
                 return
-            elif not valid:
+
+            # === display error message ===
+            elif not is_valid_input:
                 self.error_message()
 
-            self.print_input()
+            self.update_display()
 
-    def get_second_number(self):
+    def get_second_number(self) -> None:
+        """Gets the second number."""
         while True:
             user_input = self.get_input()
-            valid = self.update_number_input(user_input, "second_number")
+            is_valid_input = self.update_number_input(user_input, "second_number")
+
+            # === move on to the calculation ===
             if user_input in ["=", readchar.key.ENTER] and self.second_number:
-                self.second_number = self.second_number
-                self.print_input()
+                self.update_display()
                 return
+
+            # === clear everything ===
             elif user_input == "c":
                 self.first_number = self.arithmetic_expression = ""
                 return
-            elif not valid:
+
+            # === display error message ===
+            elif not is_valid_input:
                 self.error_message()
 
-            self.print_input()
+            self.update_display()
 
 
-    def get_result(self):
+    def perform_calculation(self) -> None:
+        """Performs the calculation and continuing with the result logic."""
+
         arithmetic_operations = {
             "+": lambda num1, num2: num1 + num2,
             "-": lambda num1, num2: num1 - num2,
             "*": lambda num1, num2: num1 * num2,
-            "/": lambda num1, num2: "ERROR" if num2 == "0" else round(num1 / num2, 10),
+            "/": lambda num1, num2: "ERROR" if num2 == "0" else round(num1 / num2, 2),
 
         }
         a = int(self.first_number) if not "." in self.first_number else float(self.first_number)
@@ -79,12 +102,11 @@ class Calculator:
 
         self.result = arithmetic_operations[self.arithmetic_expression](a, b)
 
-        if str(self.result)[-1] == "0":
+        # === turns floats with 0 as decimal to integer ===
+        if isinstance(self.result, float) and self.result.is_integer():
             self.result = int(self.result)
 
-        self.print_input()
-
-
+        self.update_display()
 
         while True:
             continue_with_result = self.get_input()
@@ -93,26 +115,28 @@ class Calculator:
                 break
             else:
                 self.error_message()
-            self.print_input()
+            self.update_display()
 
-
+        # === continue with result ===
         if continue_with_result in ["+", "-", "*", "/"] and not self.result == "ERROR":
             self.first_number = str(self.result)
             self.arithmetic_expression = continue_with_result
 
-
+        # === reset calculator ===
         elif continue_with_result == "c":
             self.first_number = ""
             self.arithmetic_expression = ""
         self.second_number = self.result = ""
 
-        self.print_input()
+        self.update_display()
 
     @staticmethod
     def get_input():
+        """Reads the keyboard."""
         return readchar.readkey()
 
-    def print_input(self):
+    def update_display(self) -> None:
+        """Formats, updates and prints the numbers, arithmetic expressions and result."""
         self.clear_terminal()
         if self.result or self.result == 0:
             display = f"{self.result}"
@@ -127,11 +151,12 @@ class Calculator:
             display = f"{self.first_number}"
         else:
             display = ""
-        print(self.ascii_art(display))
+        print(self.ascii_art_calculator(display))
 
-
-    def ascii_art(self, user_input):
-        calc = f"""
+    @staticmethod
+    def ascii_art_calculator(user_input) -> str:
+        """The ASCII calculator art wherein the display gets printed."""
+        ascii_art_string = f"""
      _____________________
     |  _________________  |
     | | {user_input.center(15) } | |
@@ -147,21 +172,21 @@ class Calculator:
     | |___|___|___| |___| |
     |_____________________|
     """
-        return (calc
+        return (ascii_art_string
         )
 
-    def run(self):
-        self.print_input()
+    def run(self) -> None:
+        """The flow."""
+        self.update_display()
         if not self.first_number:
             self.get_first_number()
 
-        elif not self.second_number:
-            self.get_second_number()
+        self.get_second_number()
 
-        elif self.second_number:
-            self.get_result()
-
+        self.perform_calculation()
 
 calc = Calculator()
 while True:
     calc.run()
+
+
